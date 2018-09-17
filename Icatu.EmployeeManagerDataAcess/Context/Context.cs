82ef5bfad1f47ad3a52c2987c021152ede24ca4a
Leonardo Cesar de Souza Lima <leocesarlima@hotmail.com>
2018-09-17 13:30:56 -0300

    commit changes solution

--- Icatu.EmployeeManagerDataAcess/Context/Context.cs
+++ Icatu.EmployeeManagerDataAcess/Context/Context.cs
@@ -5,11 +5,6 @@ namespace Icatu.EmployeeManagerDataAcess
 {
     public class Context: DbContext
     {
-        public Context() 
-            : base()
-        {
-        }
-
         public DbSet<Employee> Employee { get; set; }
         public DbSet<Department> Department { get; set; }
 
@@ -17,7 +12,7 @@ namespace Icatu.EmployeeManagerDataAcess
         {
             if (!optionsBuilder.IsConfigured)
             {
-                //optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=EmployeeManager;Trusted_Connection=True;");
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=EmployeeManager;Integrated Security=True;MultipleActiveResultSets=true;");
             }
         }
 
---
+++ Icatu.EmployeeManagerDataAcess/Migrations/20180917160541_teste.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Icatu.EmployeeManager.DataAcess.Migrations
+{
+    public partial class teste : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Department",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Mail = table.Column<string>(maxLength: 50, nullable: false),
+                    IdDepartament = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employee_Department_IdDepartament",
+                        column: x => x.IdDepartament,
+                        principalTable: "Department",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_IdDepartament",
+                table: "Employee",
+                column: "IdDepartament");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "Department");
+        }
+    }
+}
